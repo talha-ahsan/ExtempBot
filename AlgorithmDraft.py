39aae90c31ba67b7fd27390041990a16eb557c2f
benpankow <pankow.ben@gmail.com>
2015-09-02 14:26:47 -0500

    Comments

--- AlgorithmDraft.py
+++ AlgorithmDraft.py
@@ -7,27 +7,29 @@ from newspaper import Article as nArticle
 
 stopwords = set(stopwords.words('english'))
 globalWordCloud = ['Tags']
-categories = ['Country or Subject insert here']
+categories = ['Country or subject here']
+
 
 class Country:
     # Contains total words in articles inside. Total word count inside China folder, and more!
     totalWordCount = 0
     countryFolderPath = "folderpath"
-    # WordRate gives a probability out of 1000 words how many instances of the word which is the key will be present
     #wordRate and wordOccuranceCount are both maps with string keys, and integer values. wordRate / 1000 * totalWordCount = the number of times the word has been seen
     wordRate = {}
-    # We need to make sure that wordRate's keys are part of wordCloud, so we need a method to sync these repeatedly.
     wordOccuranceCount = {}
     # Where wordRate[key] = wordTotalCount[key] * 1000 / totalWordCount
 
+	
 class Article:
     # do something to create article word rate, simply create a dictionary with toal occurences, and word count, and from there calculate per keyword the word rate
     articleWordRate = {}
-    
+    articleOccuranceCount = {}
+
     def __init__ (self, url, text):
         self.articleURL = url
         self.articleBody = text
 
+		
 def updateClouds(article, country):
     # takes article keywords, and adds them to the grand list if necessary
     testwords = article.articleWordRate.keys()
@@ -40,6 +42,7 @@ def updateClouds(article, country):
             country.wordRate[word] = 0
     return
 
+	
 # compares article rate with country rate, returning the distance
 def distance(article, country):
     # make sure errors are minimized
@@ -52,48 +55,61 @@ def distance(article, country):
         distancesquare += (termdifference ** 2)
     return math.sqrt(distancesquare)
 	
+	
 # removes stop words, makes lowercase. returns a list of words in the article
 def sanitize(text):
-	# first, make lowercase
-	text = text.lower()
-	# make a list of every word
-	list = re.compile('\w+').findall(text)
-	# lambda function that removes all stopwords
-	list = filter(lambda w: not w in stopwords, list)
-	return list
+    # first, make lowercase
+    text = text.lower()
+    # make a list of every word
+    list = re.compile('\w+').findall(text)
+    # lambda function that removes all stopwords
+    list = filter(lambda w: not w in stopwords, list)
+    return list
 
+	
+# counts occurrence of each word in the article (this number is saved for category calculation later) and used to determine the word rate
 def calculateWordRate(article):
+	# get a list of words in the article
     words = sanitize(article.articleBody)
-    articleOccuranceCount = {}
+	
     totalWords = 0
     for word in words:
-        if word not in articleOccuranceCount.keys():
-            articleOccuranceCount[word] = 0
-            
-        articleOccuranceCount[word] = articleOccuranceCount[word] + 1
+        if word not in article.articleOccuranceCount.keys():
+            article.articleOccuranceCount[word] = 0
+        
+		# count each word and count the total amount of words
+        article.articleOccuranceCount[word] = article.articleOccuranceCount[word] + 1
         totalWords += 1
-
+	# calculate the rate
     for word in articleOccuranceCount.keys(): 
         article.articleWordRate[word] = (articleOccuranceCount[word] * 1000) / totalWords
 
-def mergeText(article, country):
-    wordsToAdd = sanitize(article.articleBody)
-    for word in wordsToAdd:
-        country.wordOccuranceCount[word] = country.wordOccuranceCount[word] + 1 #Can i just do += 1? ++ won't work
+		
+# add an article's word data to the country's word data
+def mergeWords(article, country):
+	# update the occurrence count for the country as a whole, adding the occurrences from the article
+    for word in article.articleOccuranceCount:
+        country.wordOccuranceCount[word] = country.wordOccuranceCount[word] + article.articleOccuranceCount[word] #Can i just do += 1? ++ won't work
         country.totalWordCount += 1
+	# now update the word rate
     for key in country.wordOccuranceCount.keys():
         country.wordRate[key] = country.wordOccuranceCount[key] * 1000 / country.totalWordCount
     return
 	
+	
 if __name__ == '__main__':
-    url = 'http://www.nytimes.com/2015/09/02/us/politics/ben-carson-advancing-in-polls-is-a-sharp-contrast-to-donald-trump.html'
+	# create a newspaper Article object
+    url = 'http://www.nytimes.com/2015/09/02/us/politics/cnn-alters-debate-criteria-which-could-help-carly-fiorina.html'
     narticle = nArticle(url)
+	# download and parse the article (this gives us the clean text and info like author, date etc)
     narticle.download()
     narticle.parse()
     text = narticle.text
     
+	# create an article object and calculate the word rate
     article = Article(url, text)
     calculateWordRate(article)
     sortedWordRate = sorted(article.articleWordRate.items(), key=operator.itemgetter(1))
     for item in sortedWordRate:
         print(item)
+	
