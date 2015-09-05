16a7792a235de41e5ada34484a1cc9969084f35f
benpankow <pankow.ben@gmail.com>
2015-09-05 14:22:24 -0500

    Removed files for Article and Category for now

--- AlgorithmDraft.py
+++ AlgorithmDraft.py
@@ -5,13 +5,108 @@ import operator #temp for testing
 import pickle
 from nltk.corpus import stopwords
 from newspaper import Article as nArticle
-from Category import Category
-from Article import Article
 
 stopwords = set(stopwords.words('english'))
 globalWordCloud = ['Tags']
 
 categories = []
+
+class Category:
+    name = ""
+    # Contains total words in articles inside. Total word count inside China folder, and more!
+    totalWordCount = 0
+    categoryFolderPath = "folderpath"
+    #wordRate and wordOccuranceCount are both maps with string keys, and integer values. wordRate / 1000 * totalWordCount = the number of times the word has been seen
+    wordRate = {}
+    wordOccuranceCount = {}
+    # Where wordRate[key] = wordOccuranceCount[key] * 1000 / totalWordCount
+
+    # All the articles contained in this category
+    articles = []
+
+    def __init__ (self, name, path):
+        self.name = name
+        self.categoryFolderPath = path
+
+    def save(self):
+        ids = []
+        for article in articles:
+            # article.save() TODO
+            ids.append(article.id)
+
+        saveFile = open(self.name + '.xcat', 'wb')
+        data = [self.name, self.totalWordCount, self.categoryFolderPath, self.wordRate, self.wordOccuranceCount, ids]
+        pickle.dump(data, saveFile)
+        saveFile.close()
+
+    # add an article's word data to the category's word data
+    def addArticle(self, article):
+        articles.append(article)
+        # update the occurrence count for the category as a whole, adding the occurrences from the article
+        for word in article.articleOccuranceCount:
+            self.wordOccuranceCount[word] = self.wordOccuranceCount[word] + article.articleOccuranceCount[word]
+            self.totalWordCount += article.articleOccuranceCount[word]
+        # now update the word rate
+        for key in self.wordOccuranceCount.keys():
+            self.wordRate[key] = self.wordOccuranceCount[key] * 1000 / self.totalWordCount
+
+    # remove an article from this category
+    def removeArticle(self, article):
+        articles.remove(article)
+        # update the occurrence count for the category as a whole, subtracting the occurrences from the article
+        for word in article.articleOccuranceCount:
+            self.wordOccuranceCount[word] = self.wordOccuranceCount[word] - article.articleOccuranceCount[word]
+            self.totalWordCount -= article.articleOccuranceCount[word]
+        # now update the word rate
+        for key in self.wordOccuranceCount.keys():
+            self.wordRate[key] = self.wordOccuranceCount[key] * 1000 / self.totalWordCount
+
+    @classmethod
+    def loadFromFile(cls, name):
+        loadFile = open(name + '.xcat', 'rb')
+        data = pickle.load(loadFile)
+        loadFile.close()
+        cat = cls(data[0], data[2])
+        cat.totalWordCount = data[1]
+        cat.wordRate = data[3]
+        cat.wordOccuranceCount = data[4]
+        ids = data[5]
+        for id in ids:
+            # article = Article.loadFromFile(id)
+            # cat.articles.append(article)
+            id = id
+
+        return cat
+        
+
+class Article:
+    id = 0
+    articleWordRate = {}
+    articleOccuranceCount = {}
+    articleURL = ""
+    articleBody = ""
+
+    def __init__ (self, url, text):
+        self.articleURL = url
+        self.articleBody = text
+
+    # counts occurrence of each word in the article (this number is saved for category calculation later) and used to determine the word rate
+    def calculateWordRate(self):
+        # get a list of words in the article
+        words = sanitize(self.articleBody)
+
+        totalWords = 0
+        for word in words:
+        # ignore all stopwords
+            if word not in stopwords:
+                if word not in self.articleOccuranceCount.keys():
+                    self.articleOccuranceCount[word] = 0
+                # count each word and count the total amount of words
+                self.articleOccuranceCount[word] = self.articleOccuranceCount[word] + 1
+                totalWords += 1
+        # calculate the rate
+        for word in self.articleOccuranceCount.keys():
+            self.articleWordRate[word] = (self.articleOccuranceCount[word] * 1000) / totalWords
 
 #Syncs article text with both global word cloud and the category's word cloud to make sure both have all possible keywords
 def updateClouds(article, category):
