639b3164d664bb9a899159f63df24496ddb89069
talhaahsan <stwyde@gmail.com>
2016-07-18 15:21:52 -0500

    restructured RSS feed setup

--- ParsingSampleCode.py
+++ ParsingSampleCode.py
@@ -7,13 +7,22 @@ from nltk.corpus import stopwords
 from newspaper import Article as nArticle
 import feedparser
 
-#An arbitrary set of feeds off the new york times RSS webpage. INCOMPLETE
-feedList = ['http://rss.nytimes.com/services/xml/rss/nyt/World.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Africa.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Europe.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/MiddleEast.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/US.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Education.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Business.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/EnergyEnvironment.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/InternationalBusiness.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/SmallBusiness.xml']
+#An arbitrary set of feeds off the new york times RSS webpage.
+nytimes = ['http://rss.nytimes.com/services/xml/rss/nyt/World.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Africa.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Europe.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/MiddleEast.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/US.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Education.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Business.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/EnergyEnvironment.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/InternationalBusiness.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/SmallBusiness.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Economy.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/MediaandAdvertising.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/YourMoney.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml', 'http://bits.blogs.nytimes.com/feed/', 'http://rss.nytimes.com/services/xml/rss/nyt/PersonalTech.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Science.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Environment.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Health.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Research.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Nutrition.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/HealthCarePolicy.xml', 'http://rss.nytimes.com/services/xml/rss/nyt/Views.xml']
+
+#WashPo RSS
+washPo = []
+
+#Daily Beast
+dBeast = []
+
+sourceList = [nytimes, washPo, dBeast]
 
 feeds = []
 
-for feed in feedList:
-    feeds.append(feedparser.parse(feed))
+for source in sourceList:
+    for feed in source:
+        feeds.append(feedparser.parse(feed))
 
 #The set of all articles in Newspaper format.
 entries = []
