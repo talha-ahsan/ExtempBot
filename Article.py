ea55d0e3113eb1a1da53fdfc17df44af2ad78885
talhaahsan <stwyde@gmail.com>
2018-02-17 22:56:55 -0600

    added init function

--- Article.py
+++ Article.py
@@ -1,3 +1,12 @@
 __author__ = 'talhaahsan and benpankow'
 from newspaper import Article as newspaperArticle
 
+class Article:
+
+    def __init__(self, rssentry):
+        self.link = rssentry.link
+        self.newspaperFile = newspaperArticle(rssentry.link)
+        self.newspaperFile.download()
+        self.newspaperFile.parse()
+        self.text = self.newspaperFile.text
+
